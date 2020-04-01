fc397298aedcf05f8507d1fb4b1667e3dd5726ad
oashtari <oashtari@gmail.com>
2020-03-31 17:30:45 -0700

    added table, added seed data

--- knexfile.js
+++ knexfile.js
@@ -43,22 +43,3 @@ module.exports = {
   }
 
 };
-
-
-// 7. do migrations creation, but then change the db to 'market' instead of 'produce'  -- line 8 there
-// we will create a 'market' db next 
-// 8. run knex migrate:latest and it will automatically create a new db called market.db3 inside 'data'
-// this is for Sqlite3 only...for Postgrest, must create a new table first
-// every change 
-// 9. can do new migration with knex migrate:make name
-// 10. all schema change should be done with migrations
-
-// module.exports = {
-
-//   development: {
-//     client: 'sqlite3', // db driver
-//     connection: {
-//       filename: './data/market.db3' // path is from root, not from file we're in, so only one dot/. is needed
-//     }, // could be an object or a string
-//     useNullAsDefault: true, // only needed for SQLite
-//   },
--- migrations/20200331165608_car-dealer.js
+++ migrations/20200331165608_car-dealer.js
@@ -1,8 +1,31 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments();
+
+        tbl.string('VIN', 128)
+            .notNullable()
+            .unique()
+            .index()
+
+        tbl.string('make', 128)
+            .notNullable()
+            .index()
+
+        tbl.string('model', 128)
+            .notNullable()
+            .index()
+
+        tbl.float('mileage')
+            .notNullable()
+            .index()
+
+        tbl.string('transmission_type', 128)
+
+        tbl.string('title_status', 128)
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('cars')
 };
