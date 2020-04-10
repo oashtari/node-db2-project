a84b46576a87e7785c0715705ca059b6841269cf
oashtari <oashtari@gmail.com>
2020-04-09 18:40:58 -0700

    finished CRUD operations

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -4,23 +4,26 @@ const express = require('express');
 const db = require('../data/connections');
 
 const router = express.Router();
+// ALL THE WRONG ROUTES
+// MUST ADDRESS
 
 router.get('/', (req, res) => {
-    db('fruits')
-        .then(fruits => {
-            res.json(fruits);
+    db('cars')
+        .then(cars => {
+            res.json(cars);
         })
         .catch(err => {
-            res.status(500).json({ message: 'Failed to retrieve fruits' });
+            res.status(500).json({ message: 'Failed to retrieve cars' });
         });
 });
 
 router.get('/:id', (req, res) => {
     const { id } = req.params;
 
-    db('fruits').where({ id }).first()
-        .then(fruit => {
-            res.json(fruit);
+    db('cars')
+        .where({ id }).first()
+        .then(car => {
+            res.json(car);
         })
         .catch(err => {
             res.status(500).json({ message: 'Failed to retrieve fruit' });
@@ -28,13 +31,15 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    const fruitData = req.body;
-    db('fruits')
-        .insert(fruitData, 'id')
+    const carData = req.body;
+
+    db('cars')
+        .insert(carData, 'id')
         .then(ids => {
-            db('fruits').where({ id: ids[0] })
-                .then(newFruitEntry => {
-                    res.status(201).json(newFruitEntry);
+            db('cars')
+                .where({ id: ids[0] })
+                .then(newCarEntry => {
+                    res.status(201).json(newCarEntry);
                 });
         })
         .catch(err => {
@@ -42,5 +47,47 @@ router.post('/', (req, res) => {
             res.status(500).json({ message: "Failed to store data" });
         });
 });
+
+
+router.put('/:id', (req, res) => {
+    const changes = req.body;
+    db('cars')
+        .where({ id: req.params.id })
+        .update(changes) // needs two arguments
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({ message: "update success" })
+            } else {
+                res.status(404).json({ message: "account not found" })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "argh error" })
+        })
+});
+
+router.delete('/:id', (req, res) => {
+    db('cars')
+        .where({ id: req.params.id })
+        .del() // delete the records
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({ message: "delete success" })
+            } else {
+                res.status(404).json({ message: "account not found" })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "argh error" })
+        })
+});
+
+
+// Add a query string option to your GET /api/accounts endpoint. 
+// The query string may contain limit, sortby and sortdir keys. 
+// If these keys are provided, use these values to limit and sort 
+// the accounts which are selected from the database. 
+// Reference the docs for sorting and limiting in knex.
+
 
 module.exports = router;
